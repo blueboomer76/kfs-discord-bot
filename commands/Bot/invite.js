099dde7308cd62ec319c701bdfa62a1438407261
blueboomer76 <39141684+blueboomer76@users.noreply.github.com>
2018-05-19 17:17:33 -0700

    Move invite command and add contents

--- commands/Bot/invite.js
+++ commands/Bot/invite.js
@@ -8,7 +8,21 @@ module.exports.run = async (bot, message, args) => {
 	.setFooter("Made by blueboomer#4939")
 	.addField("Bot Invite", "[Go!](https://discordapp.com/api/oauth2/authorize?client_id=429807759144386572&permissions=403041398&scope=bot")
 	.addField("Kendra's server", "[Go!](https://discord.gg/yB8TvWU)")
+	.addField("Github", "[Go!](https://github.com/blueboomer76/KendraBot)")
 	);
+}
+
+module.exports.config = {
+	"aliases": [],
+	"cooldown": {
+		"waitTime": 30000,
+		"type": "guild"
+	},
+	"guildOnly": false,
+	"perms": {
+		"level": 0,
+		"reqPerms": null
+	}
 }
 
 module.exports.help = {
