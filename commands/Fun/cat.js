835d151608582d4f6f3b65ac52062be03547079c
blueboomer76 <39141684+blueboomer76@users.noreply.github.com>
2018-09-15 18:45:53 -0700

    Add commands, request and other dependencies

--- commands/Fun/cat.js
+++ commands/Fun/cat.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const Command = require("../../structures/command.js");
-const superagent = require("superagent");
+const request = require("request");
 
 class CatCommand extends Command {
 	constructor() {
@@ -22,15 +22,17 @@ class CatCommand extends Command {
 	}
 	
 	async run(bot, message, args, flags) {
-		let {body} = await superagent
-		.get("http://aws.random.cat/meow");
-	  
-		message.channel.send(new Discord.RichEmbed()
-		.setTitle("Here's your random cat!")
-		.setColor(Math.floor(Math.random() * 16777216))
-		.setFooter("From random.cat")
-		.setImage(body.file)
-		);
+		request.get("http://aws.random.cat/meow", (err, res) => {
+			if (err) return message.channel.send(`Could not request to random.cat: ${err.message}`);
+			if (!res) return message.channel.send("No response was received from random.cat.");
+			if (res.statusCode >= 400) return message.channel.send(`The request to random.cat failed with status code ${res.statusCode} (${res.statusMessage})`);
+			message.channel.send(new Discord.RichEmbed()
+			.setTitle("Here's your random cat!")
+			.setColor(Math.floor(Math.random() * 16777216))
+			.setFooter("From random.cat")
+			.setImage(JSON.parse(res.body).file)
+			);
+		})
 	}
 }
 
