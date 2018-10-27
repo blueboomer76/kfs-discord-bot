08927f005e9a63707dc3bf8c46e169f70ab4acb7
blueboomer76 <39141684+blueboomer76@users.noreply.github.com>
2018-10-26 19:04:34 -0700

    More image commands and bug fixes

---
+++ utils/imageManager.js
@@ -0,0 +1,48 @@
+const Jimp = require("jimp");
+
+module.exports = {
+	getRecentImage: message => {
+		return new Promise(async (resolve, reject) => {
+			await message.channel.fetchMessages({limit: 25})
+			.then(msgs => {
+				msgs = msgs.array();
+				let imageURL;
+				for (const msg of msgs) {
+					if (msg.attachments.size > 0) {
+						imageURL = msg.attachments.last().url;
+						break;
+					} else {
+						let lastEmbed = msg.embeds[msg.embeds.length - 1];
+						if (lastEmbed && lastEmbed.image) {
+							imageURL = lastEmbed.image.url;
+							break;
+						} else if (lastEmbed && lastEmbed.type == "image") {
+							imageURL = lastEmbed.url;
+							break;
+						}
+					}
+				}
+				if (imageURL) {
+					resolve(imageURL);
+				} else {
+					reject("No image attachment found in recent messages.")
+				}
+			})
+			.catch(err => reject("Failed while trying to fetch messages:\n" + err.stack))
+		})
+	},
+	postImage: (msg, img, fileName) => {
+		img.getBufferAsync(Jimp.MIME_PNG)
+		.then(imgToSend => {
+			msg.channel.send({
+				files: [{
+					attachment: imgToSend,
+					name: fileName
+				}]
+			})
+		})
+		.catch(err => {
+			msg.channel.send("Failed to generate the image.")
+		})
+	}
+}
