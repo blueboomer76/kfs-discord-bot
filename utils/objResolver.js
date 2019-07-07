9dd327b350987bb13d742161a1b56ed331c35a9f
blueboomer76 <39141684+blueboomer76@users.noreply.github.com>
2019-07-06 17:26:41 -0700

    Add color argument type and help subcommand

--- utils/objResolver.js
+++ utils/objResolver.js
@@ -1,8 +1,17 @@
 const {fetchMembers} = require("../modules/memberFetcher.js"),
+	convert = require("color-convert"),
 	fs = require("fs"),
 	twemoji = require("twemoji");
 
-const emojiRegex = /<a?:[0-9A-Za-z_]{2,}:\d{17,19}>/,
+const colorRegexes = [
+		/#?[0-9a-f]{6}/i,
+		/rgb\((\d{1,3},){2}\d{1,3}\)/i,
+		/hsl\((\d{1,3},){2}\d{1,3}\)/i,
+		/decimal:\d{1,8}/i,
+		/(\d{1,3},){2}\d{1,3}/,
+		/^[a-z]+/i
+	],
+	emojiRegex = /<a?:[0-9A-Za-z_]{2,}:\d{17,19}>/,
 	memberRegex = /<@!?\d{17,19}>/;
 
 async function getMember(message, id) {
@@ -37,6 +46,36 @@ module.exports.resolve = async (bot, message, obj, type, params) => {
 
 			list = guildChannels.array().filter(chnl => chnl.name.toLowerCase().includes(lowerObj));
 			return list.length > 0 ? list : null;
+		case "color":
+			const objWithNoSpaces = lowerObj.replace(/[ %]/g, "").toLowerCase();
+			let i, colorMatch;
+			for (i = 0; i < colorRegexes.length; i++) {
+				const matched = objWithNoSpaces.match(colorRegexes[i]);
+				if (matched) {colorMatch = matched[0]; break}
+			}
+			switch (i) {
+				case 0: // #rrggbb or rrggbb | e.g. #112233 or 112233
+					return parseInt(colorMatch.replace("#", ""), 16);
+				case 1: // rgb(r,g,b) | e.g. rgb(1,2,3)
+					const rgbValues = colorMatch.slice(4, colorMatch.length - 1).split(",");
+					return rgbValues.some(value => value > 255) ? null : parseInt(rgbValues[0]) * 65536 + parseInt(rgbValues[1]) * 256 + parseInt(rgbValues[2]);
+				case 2: // hsl(h,s,l) | e.g. hsl(1,2,3)
+					const hslValues = colorMatch.slice(4, colorMatch.length - 1).split(",");
+					if (hslValues[0] >= 360 || hslValues[1] > 100 || hslValues[2] > 100) return null;
+					const hslRgbValues = convert.hsl.rgb(hslValues);
+					return hslRgbValues[0] * 65536 + hslRgbValues[1] * 256 + hslRgbValues[2];
+				case 3: // decimal:number | e.g. decimal:1234
+					const decimalValue = parseInt(colorMatch.slice(8));
+					return decimalValue < 16777216 ? decimalValue : null;
+				case 4: // r,g,b | e.g. 1,2,3
+					const rgbValues2 = colorMatch.split(",");
+					return rgbValues2.some(value => value > 255) ? null : parseInt(rgbValues2[0]) * 65536 + parseInt(rgbValues2[1]) * 256 + parseInt(rgbValues2[2]);
+				case 5: // CSS color name | e.g. blue
+					const nameRgbValues = convert.keyword.rgb(colorMatch);
+					return nameRgbValues ? nameRgbValues[0] * 65536 + nameRgbValues[1] * 256 + nameRgbValues[2] : null;
+				default:
+					return null;
+			}
 		case "command":
 			return bot.commands.get(lowerObj) || bot.commands.get(bot.aliases.get(lowerObj)) || null;
 		case "emoji":
