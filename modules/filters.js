675761fd7e850cf47e2c8dece090c1562e7c2aa1
blueboomer76 <39141684+blueboomer76@users.noreply.github.com>
2019-11-25 10:18:01 -0800

    Add new command flags and filters

--- modules/filters.js
+++ modules/filters.js
@@ -7,14 +7,34 @@ module.exports.applyJimpFilter = (img, filter, options) => {
 		case "blur":
 			img.blur(options.blur || getPixelFactor(img));
 			break;
-		case "colorify":
+		case "colorify": {
+			const r = options.colors[0],
+				g = options.colors[1],
+				b = options.colors[2],
+				cr = (255 - r) / 1.5,
+				cg = (255 - g) / 1.5,
+				cb = (255 - b) / 1.5;
+			const intensityFactor = 1 - (options.intensity - 1) / 9,
+				lowerBound = intensityFactor * -0.5,
+				upperBound = intensityFactor * 1.5,
+				rangeFactor = (upperBound - lowerBound) / 765;
+
+			// The constant c is always -0.5 to 1.5
 			img.scan(0, 0, img.bitmap.width, img.bitmap.height, (x, y, i) => {
-				const avg = (img.bitmap.data[i] + img.bitmap.data[i+1] + img.bitmap.data[i+2]) / 3;
-				img.bitmap.data[i] = (avg / 255) * (255 - options.colors[0]) + options.colors[0];
-				img.bitmap.data[i+1] = (avg / 255) * (255 - options.colors[1]) + options.colors[1];
-				img.bitmap.data[i+2] = (avg / 255) * (255 - options.colors[2]) + options.colors[2];
+				const c = lowerBound + (img.bitmap.data[i] + img.bitmap.data[i+1] + img.bitmap.data[i+2]) * rangeFactor;
+				if (c < 0) {
+					const c1 = -c + 0.5;
+					img.bitmap.data[i] = c1 * r;
+					img.bitmap.data[i+1] = c1 * g;
+					img.bitmap.data[i+2] = c1 * b;
+				} else {
+					img.bitmap.data[i] = c * cr + r;
+					img.bitmap.data[i+1] = c * cg + g;
+					img.bitmap.data[i+2] = c * cb + b;
+				}
 			});
 			break;
+		}
 		case "deepfry":
 			img.scan(0, 0, img.bitmap.width, img.bitmap.height, (x, y, i) => {
 				img.bitmap.data[i] = img.bitmap.data[i] < 144 ? 0 : 255;
@@ -41,6 +61,43 @@ module.exports.applyJimpFilter = (img, filter, options) => {
 		case "pixelate":
 			img.pixelate(options.pixels || getPixelFactor(img));
 			break;
+		case "rainbowify": {
+			const centerX = img.bitmap.width / 2,
+				centerY = img.bitmap.height / 2;
+			const stretchFactor = img.bitmap.width / img.bitmap.height,
+				maxDistance = Math.sqrt(Math.pow(centerX, 2) + Math.pow(centerY, 2));
+
+			// The constant c is always -0.5 to 1.5
+			img.scan(0, 0, img.bitmap.width, img.bitmap.height, (x, y, i) => {
+				const angle = Math.atan2((y - centerY) * stretchFactor, x - centerX) * 180 / Math.PI,
+					adjustedAngle = (angle + 450) % 360;
+				const rawR = Math.max(Math.min(255 / 60 * Math.abs(adjustedAngle - 180) - 255, 255), 0),
+					rawG = Math.max(Math.min(-255 / 60 * Math.abs(adjustedAngle - 120) + 510, 255), 0),
+					rawB = Math.max(Math.min(-255 / 60 * Math.abs(adjustedAngle - 240) + 510, 255), 0);
+
+				const currentDistance = Math.sqrt(Math.pow(x - centerX, 2) + Math.pow(y - centerY, 2)),
+					distanceFactor = 1 - currentDistance / maxDistance;
+				const r = (255 - rawR) * distanceFactor + rawR,
+					g = (255 - rawG) * distanceFactor + rawG,
+					b = (255 - rawB) * distanceFactor + rawB,
+					cr = (255 - r) / 1.5,
+					cg = (255 - g) / 1.5,
+					cb = (255 - b) / 1.5;
+
+				const c = (img.bitmap.data[i] + img.bitmap.data[i+1] + img.bitmap.data[i+2]) / 382.5 - 0.5;
+				if (c < 0) {
+					const c1 = -c + 0.5;
+					img.bitmap.data[i] = c1 * r;
+					img.bitmap.data[i+1] = c1 * g;
+					img.bitmap.data[i+2] = c1 * b;
+				} else {
+					img.bitmap.data[i] = c * cr + r;
+					img.bitmap.data[i+1] = c * cg + g;
+					img.bitmap.data[i+2] = c * cb + b;
+				}
+			});
+			break;
+		}
 		case "randomcrop": {
 			const baseX1 = Math.random() * 0.25,
 				baseY1 = Math.random() * 0.25,
@@ -49,12 +106,8 @@ module.exports.applyJimpFilter = (img, filter, options) => {
 				newY2Offset = offsetSum < 0.1 ? 0.1 - offsetSum : 0,
 				baseY2 = (1 - newY2Offset) - Math.random() * (0.25 - newY2Offset);
 
-			img.crop(
-				Math.floor(baseX1 * img.bitmap.width),
-				Math.floor(baseY1 * img.bitmap.height),
-				Math.floor(baseX2 * img.bitmap.width),
-				Math.floor(baseY2 * img.bitmap.height)
-			);
+			img.crop(baseX1 * img.bitmap.width, baseY1 * img.bitmap.height,
+				baseX2 * img.bitmap.width, baseY2 * img.bitmap.height);
 			break;
 		}
 		case "resize":
